b9bb6e4e9f0f0d3bc9716c3153efb902baed2656
Mitch Patenaude <mitch@mitchpatenaude.net>
2025-01-28 18:02:55 -0800

    Not useful

--- release_mobile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-rm -f GooeyLife.aab
-fyne release --keyStore ~/.keystore --keyName apksigning --target android
-
-rm -f GooeyLife.ipa
-fyne release --target ios --certificate "iPhone Distribution: Mitchell Ross Patenaude (W245SMS7LR)" --profile GooeyLifeDistroProfile
