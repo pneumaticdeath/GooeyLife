7dcf725757269be274e60a7dea451633580ad06b
Mitch Patenaude <mitch@mitchpatenaude.net>
2025-01-28 17:34:44 -0800

    Mobile release script more robust

--- release_mobile.sh
+++ release_mobile.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+rm -f GooeyLife.aab
 fyne release --keyStore ~/.keystore --keyName apksigning --target android
 
+rm -f GooeyLife.ipa
 fyne release --target ios --certificate "iPhone Developer: Mitchell Ross Patenaude (VQWB76ZRJG)" --profile GooeyLifeDevProfile
